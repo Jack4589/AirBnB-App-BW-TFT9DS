33e131283f3fc5ef0418275f5074ff40361a7768
hillarykhan <35015753+hillarykhan@users.noreply.github.com>
2021-04-26 19:27:54 -0400

    Update simple_model.py

--- Application/simple_model.py
+++ Application/simple_model.py
@@ -2,28 +2,31 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 
 data_path = 'https://github.com/Jack4589/AirBnB-App-BW-TFT9DS/raw/main/Data/listings.csv'
+
+def create_model(data_path):
+
+    nyc_simple = pd.read_csv(data_path)
+
+    # Set target
+    target = "price"
+
+    # Create target vector
+    y = nyc_simple[target]
+
+    # Create simple (1 variable) feature matrix (vector)
+    X = nyc_simple.drop(columns=[target])
+    X_feats = ["availability_365"]
+    X_simple = X[X_feats]
+
+    # Instantiate model
+    model = LinearRegression()
+
+    # Fit model
+    model_lr = model.fit(X_simple, y)
     
-nyc_simple = pd.read_csv(data_path)
-
-# Set target
-target = "price"
-
-# Create target vector
-y = nyc_simple[target]
-
-# Create simple (1 variable) feature matrix (vector)
-X = nyc_simple.drop(columns=[target])
-X_feats = ["availability_365"]
-X_simple = X[X_feats]
-
-# Instantiate model
-model = LinearRegression()
-
-# Fit model
-model_lr = model.fit(X_simple, y)
-
-# make up value for prediction
-user_input = 123
-
-# Store prediction
-prediction = model_lr.predict(user_input)
+    return model_lr
+  
+def predict_price(user_input)
+    # Store prediction
+    prediction = model_lr.predict(user_input)
+    return prediction
