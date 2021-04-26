3c6f9fa2d070088c6709d82f23fc4a95a14667b5
hillarykhan <35015753+hillarykhan@users.noreply.github.com>
2021-04-26 14:14:25 -0400

    Very simple linear regression model

---
+++ Application/simple_model.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+data_path - 'https://github.com/Jack4589/AirBnB-App-BW-TFT9DS/blob/main/Data/listings.csv'
+    
+nyc_simple = pd.read_csv(data_path)
+
+# Set target
+target = "price"
+
+# Create target vector
+y = nyc_simple[target]
+
+# Create simple (1 variable) feature matrix (vector)
+X = nyc_simple.drop(columns=[target])
+X_feats = ["availability_365"]
+X_simple = X[X_feats]
+
+# Instantiate model
+model = LinearRegression()
+
+# Fit model
+model_lr = model.fit(X_simple, y)
+
+# make up value for prediction
+user_input = 123
+
+# Store prediction
+prediction = model_lr.predict(user_input)
