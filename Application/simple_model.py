3736971958f8a38d88b7ff87772f52498d5b8286
hillarykhan <35015753+hillarykhan@users.noreply.github.com>
2021-04-27 14:11:39 -0400

    Increased number of params used in lr model

--- Application/simple_model.py
+++ Application/simple_model.py
@@ -1,28 +1,46 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from category_encoders import OneHotEncoder
 
-data_path = 'https://github.com/Jack4589/AirBnB-App-BW-TFT9DS/raw/main/Data/listings.csv'
+data_path = 'https://github.com/Jack4589/AirBnB-App-BW-TFT9DS/raw/main/Data/listings.csv.gz'
 
 
-nyc_simple = pd.read_csv(data_path)
+nyc_large = pd.read_csv(data_path, compression='gzip', 
+                        header=0, sep=',', quotechar='"', error_bad_lines=False,
+                        index_col='id')
 
-# Set target
-target = "price"
+# Set columns to keep, designate features and target
+columns_keep = ["availability_365", "minimum_nights", "room_type", "bathrooms", "bedrooms", "beds", "price"]
+target = ["price"]
+features = ["availability_365", "minimum_nights", "room_type", "bathrooms", "bedrooms", "beds"]
+
+nyc = nyc_large[columns_keep]
+
+# Drop NAs
+nyc = nyc.dropna()
+
+# Convert price from object to float
+nyc["price"] = nyc["price"].replace('[\$,]', '', regex=True).astype(float)
+
+# Convert bathrooms, bedrooms, and beds from float to int
+nyc = nyc.astype({'bathrooms': int, 'bedrooms': int, 'beds': int})
 
 # Create target vector
-y = nyc_simple[target]
+y = nyc[target]
 
-# Create simple (1 variable) feature matrix (vector)
-X = nyc_simple.drop(columns=[target])
-X_feats = ["availability_365", "minimum_nights"]
-X_simple = X[X_feats]
+# Create feature matrix 
+X = nyc[features]
+
+# Instantiate and apply One Hot Encoder to room_type
+ohe = OneHotEncoder(use_cat_names=True)
+X_transform = ohe.fit_transform(X)
 
 # Instantiate model
 model = LinearRegression()
 
 # Fit model
-model_lr = model.fit(X_simple, y)
+model_lr = model.fit(X_transform, y)
     
 
   
